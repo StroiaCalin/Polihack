ed4266dee67c08d090292a3f81cab47d9cdddf5a
Anton-Calin Stroia <126579092+StroiaCalin@users.noreply.github.com>
2025-07-19 10:16:30 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        LuxoraMedium: ['LuxoraMedium', 'sans-serif'],  // Add your custom font here
+      },
+    },
+  },
+  plugins: [],
+}
